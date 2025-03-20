996135aa341b1cb3d883f9044f93eacd28e618e6
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-03-20 07:51:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import MemoryGame from './MemoryGame'
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
