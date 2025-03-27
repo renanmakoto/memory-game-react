b2bdaff0c7020651affeb9a8656ea411bb6f3ffa
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-03-27 07:34:52 -0400

    Update game.js

--- src/game/game.js
+++ src/game/game.js
@@ -47,7 +47,7 @@ let game = {
 
     clearCards: function () {
         this.firstCard = null
-        this.secondCard = null;
+        this.secondCard = null
         this.lockMode = false;
     },
     
